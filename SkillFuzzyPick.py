3de6ccd74e96bf0f36419e0764465c59aaeed788
salt_laptop\siomizu <t314013@gmail.com>
2017-10-23 13:58:09 +0900

    fuzzypick回収

--- SkillFuzzyPick.py
+++ SkillFuzzyPick.py
@@ -22,7 +22,7 @@ for line in lines:
 logs=[]
 
 for i in range(len(playlog)):
-    text=str(playlog[i])
+    text=str(playlog[i])#playlog[i]が今見ているところ
     node = tagger.parseToNode(text)
     while node:
         for w in skills:
@@ -33,4 +33,35 @@ for i in range(len(playlog)):
                     temp.append(playlog[i+n-3])
                 logs.append(temp)
                 print(temp)
+                for n in range(3):
+                    flag=0
+                    checktxt=temp[4+n]
+                    node2=tagger.parseToNode(checktxt)
+                    while node2:
+                        if node.surface.count("成功"):
+                            flag=1
+                            break
+                        if node.surface.count("失敗"):
+                            flag=2
+                            break
+                        node2=node2.next
+                if flag==0:
+                    for n in range(3-n):
+                        checktxt=temp[3-n]
+                        node2=tagger.parseToNode(checktxt)
+                        playerName=node2.surface
+                        while node2:
+                            if node2.surface.isdigit():
+                                filename=playerName+".txt"
+                                ld = open(filename)
+                                lines = ld.readlines()
+                                playerSkill=[]
+                                for line in lines:
+                                    playerSkill.append(line.split())
+                                for z in playerSkill:
+                                    if node.surface.count(z[0])):
+                                        if int(node2.surface)<z[1]:
+                                            flag=1
+                                        else:
+                                            
         node=node.next
