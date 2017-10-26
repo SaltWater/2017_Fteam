e3298be0cc204dba503d926eac37de42ac3399dc
salt_laptop\siomizu <t314013@gmail.com>
2017-10-26 15:18:21 +0900

    fuzzypick仮完成版 対象は正確に取れず

--- SkillFuzzyPick.py
+++ SkillFuzzyPick.py
@@ -1,9 +1,83 @@
 import random
 import sys
 import MeCab
+import re
 
+def pickup(temp,w):
+    tagger = MeCab.Tagger("mecabrc")
+    tagger.parse('')
+    flag=0
+    ss=0
+    returnText=""
+    returnText1=""
+    returnText2=""
+    node=tagger.parseToNode(str(temp[3]))
+
+    
+    for n in range(4):
+        checktxt=str(temp[3+ss])
+        node=tagger.parseToNode(checktxt)
+        while node:
+#            print("主語:"+returnText)
+#            print("探査単語    "+node.surface)
+#            print("flag:"+str(flag))
+            if node.surface.count(":") or node.surface.count("："):
+                returnText=node.prev.surface
+                returnText+="は"
+            if node.surface.count("に") or node.surface.count("を"):
+                node2=node.prev
+                if node2.surface.count(w[0]):
+                    break
+                if node2.prev.surface.count("の"):
+                    returnText1+=node2.prev.surface
+                    returnText1+=node2.surface
+                returnText1+=node2.surface
+            if node.surface.count("成功"):
+                flag=1
+            if node.surface.count("失敗"):
+                flag=2
+            node=node.next
+        ss=ss+1
+        if flag!=0:
+            break
+
+    if flag==0:
+        for n in range(3):
+            checktxt=str(temp[2-n])
+            node=tagger.parseToNode(checktxt)
+            while node:
+                if node.surface.isdigit():
+                    filename="playerskill.txt"
+                    ld = open(filename)
+                    lines = ld.readlines()
+                    playerSkill=[]
+                    for line in lines:
+                        playerSkill.append(line.split())
+                        for z in playerSkill:
+                            if node.surface.count(z[0]):
+                                if int(node.surface)<=z[1]:
+                                    flag=1
+                                else:
+                                    flag=2
+                node=node.next
+    if flag==1:
+        returnText2=w[1]
+    if flag==2:
+        returnText2=w[2]
+    if flag==0:
+        returnText2=w[3]
+
+#    if returnText1=="":
+        
+    
+    returnText+=returnText1
+    returnText+=returnText2
+    
+    return returnText
+    
 tagger = MeCab.Tagger("mecabrc")
 tagger.parse('')
+
 
 file="playlog.txt"
 ld = open(file)
@@ -21,47 +95,29 @@ for line in lines:
 
 logs=[]
 
+f = open('out.txt', 'w') # 書き込みモードで開く
+
 for i in range(len(playlog)):
     text=str(playlog[i])#playlog[i]が今見ているところ
     node = tagger.parseToNode(text)
     while node:
         for w in skills:
             if node.surface.count(w[0]):
-                print(node.surface)
+#                print("検出技能:"+node.surface)
+#                print(playlog[i])
                 temp=[]
                 for n in range(7):
                     temp.append(playlog[i+n-3])
                 logs.append(temp)
-                print(temp)
-                for n in range(3):
-                    flag=0
-                    checktxt=temp[4+n]
-                    node2=tagger.parseToNode(checktxt)
-                    while node2:
-                        if node.surface.count("成功"):
-                            flag=1
-                            break
-                        if node.surface.count("失敗"):
-                            flag=2
-                            break
-                        node2=node2.next
-                if flag==0:
-                    for n in range(3-n):
-                        checktxt=temp[3-n]
-                        node2=tagger.parseToNode(checktxt)
-                        playerName=node2.surface
-                        while node2:
-                            if node2.surface.isdigit():
-                                filename=playerName+".txt"
-                                ld = open(filename)
-                                lines = ld.readlines()
-                                playerSkill=[]
-                                for line in lines:
-                                    playerSkill.append(line.split())
-                                for z in playerSkill:
-                                    if node.surface.count(z[0]):
-                                        if int(node2.surface)<z[1]:
-                                            flag=1
-                                        else:
-                                            flag=2
-    node=node.next
+#                print (temp)
+                rrr=str(pickup(temp,w))
+#                print(rrr)
+                f.writelines(rrr)
+                f.write("\n")
+        node=node.next
+    text=text+"\n"
+    text=re.sub('\[',"",text)
+    text=re.sub('\]',"",text)
+    text=re.sub('\'',"",text)
+    f.writelines(text)
+f.close()
