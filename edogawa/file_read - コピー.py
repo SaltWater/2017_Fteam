718824dbb83ad054cdff6b583da204f432014c2b
KAZUO KOUHEI <t314013@gmail.com>
2017-11-21 23:11:55 +0900

    gitignore変更

--- edogawa/file_read - コピー.py
+++ edogawa/file_read - コピー.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import zipfile
 
 def find_all_files(directory):
@@ -11,10 +10,14 @@ def find_all_files(directory):
 for file in find_all_files(os.getcwd()):
     if file.endswith(".zip"):
         print (file)
-        up2path=os.path.dirname(os.path.dirname(file))
+        #up2path=os.path.dirname(os.path.dirname(file))
         with zipfile.ZipFile(file, 'r') as post:
             for info in post.infolist():
                 print(info.filename)
-                outpath=os.path.join(up2path,info.filename)
-                print(outpath)
-                shutil.copyfile(outpath,info.filename)
+                if info.filename.endswith(".txt"):
+                    f=open(os.path.join(file,info.filename),"r",encoding="utf-8")
+                    lines=f.readlines()
+                    for line in lines:
+                        print(line)
+                #print(post.read(info.filename))
+                
